9a70937f66a56c8639452427adfba1e1bff1cf84
Ubugeeei <ubuge1122@gamil.com>
2022-11-04 01:18:39 +0900

    gats

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+trait LendingIterator {
+    type Item<'a>
+    where
+        Self: 'a;
+
+    fn next<'a>(&'a mut self) -> Option<Self::Item<'a>>;
+}
+
+struct Iterable {
+    cursor: usize,
+    value: Vec<i32>,
+}
+
+impl LendingIterator for Iterable {
+    type Item<'a> = &'a i32;
+
+    fn next<'a>(&'a mut self) -> Option<Self::Item<'a>> {
+        if self.cursor < self.value.len() {
+            let result = &self.value[self.cursor];
+            self.cursor += 1;
+            Some(result)
+        } else {
+            None
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut iterable = Iterable {
+        cursor: 0,
+        value: vec![1, 2, 3],
+    };
+
+    while let Some(value) = iterable.next() {
+        println!("{}", value);
+    }
 }
